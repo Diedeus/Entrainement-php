6ebb3bc91392a370475b3c936247094bce997363
= <palerm39@gmail.com>
2023-04-26 14:35:22 +0200

    2ecommit

--- entrainement2.php
+++ entrainement2.php
@@ -1,6 +1,6 @@
 <?php
 
-class voiture{
+class voiture{                                          //! creation de la class voiture et de ses objets
     public $couleur = "red";
     public $marque = "Audi";
     public $model = "R8";
@@ -8,3 +8,9 @@ class voiture{
 $voitureEnCours = new voiture;
 
 echo "la marque est: $voitureEnCours->marque ";
+$voitureEnCours->couleur = "noire";                     //! changement de valeur d'un des objets de la class voiture
+echo "la marque est $voitureEnCours->couleur";
+echo "la marque est $voitureEnCours->marque $voitureEnCours->couleur";
+$nbrRoue = "";
+$voitureEnCours->nbrRoue = "4";
+echo "le nombre de roue est de $voitureEnCours->nbrRoue";
